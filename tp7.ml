d59ad2f83dd882c554919a056b7064dad7c3a174
Benabbi Yannis <ybenabbi@gambit.enseeiht.fr>
2020-01-13 20:18:34 +0100

    Factorisation de Raquette

--- tp7.ml
+++ tp7.ml
@@ -78,61 +78,121 @@ module type Frame =
 
 module Parametres =
 struct
-  let long_raq = 100.
-  let h_raq = 20.
-  let rayon_ball = 5
-  let pos_y_raq = 50
+
+  (* Paramètres de l'écran *)
+  let long_ecran = 800.
+  let haut_ecran = 600.
+
+
+
+  (* Blocs *)
   let largeur_bloc = 100
   let hauteur_bloc = 20
-  let i = 0
   let blocs = [(100, 500); (500,500)]
+end
+
+module Balle =
+struct
+
+  (* Balle *)
+  let rayon_ball = 5
+
+
+  let coord_rel x sens = x + rayon_ball * sens
+  let xd x = coord_rel x 1
+  let xg x = coord_rel x (-1)
+  let yh y = coord_rel y 1
+  let yb y = coord_rel y (-1)
+
+
+end
+
+module Raquette =
+struct
+
+  include Parametres
+
+  let long_raq = 100.
+  let hraq = 20.
+  let yraq = 50
+
+  let get_xraq = 
+    (fun xsouris ->
+      if xsouris > int_of_float (long_ecran -. long_raq/.2.) then int_of_float (long_ecran -. long_raq/.2.)
+      else if xsouris < int_of_float (long_raq/.2.) then int_of_float (long_raq/.2.)
+      else xsouris)
+
+end
+
+module All =
+struct
+
+  include Parametres
+  include Raquette
+  include Balle
+
 end
 
 module Drawing (F : Frame) =
   struct
 
-  include Parametres
+  include All
 
-    let draw r =
-      let ref_r = ref r in
-      let ref_handler_alrm = ref Sys.(Signal_handle (fun _ -> ())) in
-      let ref_handler_int  = ref Sys.(Signal_handle (fun _ -> ())) in
-      let handler_alrm i =
-        begin
-          match Flux.uncons !ref_r with
-          | None                          ->
-             begin
-               Sys.(set_signal sigalrm !ref_handler_alrm);
-               Sys.(set_signal sigint  !ref_handler_int)
-             end
-          | Some (((x, y), (dx, dy)), r') ->
-             begin
-               (*Format.printf "r=(%f, %f); dr = (%f, %f)@." x y dx dy;*)
-               Graphics.clear_graph ();
-               Graphics.draw_circle (int_of_float x) (int_of_float y) rayon_ball;
-               let (xsouris, _) = Graphics.mouse_pos() in
-               Graphics.fill_rect (int_of_float (float_of_int xsouris -. (long_raq)/.2.)) pos_y_raq (int_of_float long_raq) (int_of_float h_raq);
-               List.iter (fun (x, y) -> Graphics.fill_rect x y largeur_bloc hauteur_bloc) blocs;
-               Graphics.synchronize ();
-               (*ignore (read_line ());*)
-               ref_r := r'
-             end
-        end in
-      let handler_int i =
-        begin
-          ref_r := Flux.vide
-        end in
+  let draw r =
+
+    let ref_r = ref r in
+    let ref_handler_alrm = ref Sys.(Signal_handle (fun _ -> ())) in
+    let ref_handler_int  = ref Sys.(Signal_handle (fun _ -> ())) in
+    let handler_alrm i =
       begin
-        let (inf_x, sup_x) = F.box_x in
-        let (inf_y, sup_y) = F.box_y in
-        let size_x = int_of_float (sup_x -. inf_x) in
-        let size_y = int_of_float (sup_y -. inf_y) in
-        Graphics.open_graph (Format.sprintf " %dx%d" size_x size_y);
-        Graphics.auto_synchronize false;
-        Sys.(ref_handler_alrm := signal sigalrm (Signal_handle handler_alrm));
-        Sys.(ref_handler_int  := signal sigint  (Signal_handle handler_int));
-        Unix.(setitimer ITIMER_REAL { it_interval = F.dt; it_value = F.dt })
-      end    
+        match Flux.uncons !ref_r with
+        | None                          ->
+            begin
+              Sys.(set_signal sigalrm !ref_handler_alrm);
+              Sys.(set_signal sigint  !ref_handler_int)
+            end
+        | Some (((x, y), (dx, dy)), r') ->
+            begin
+            (* Format.printf "r=(%f, %f); dr = (%f, %f)@." x y dx dy;*)
+            Graphics.clear_graph ();
+
+
+            (* Définition des fonctions graphiques *)
+            
+            let draw_raquette =
+              let x = get_xraq (fst (Graphics.mouse_pos ())) in
+                Graphics.fill_rect (int_of_float (float_of_int x -. (long_raq)/.2.)) yraq (int_of_float long_raq) (int_of_float hraq);
+            in
+
+            let draw_balle =
+              Graphics.draw_circle (int_of_float x) (int_of_float y) rayon_ball;
+            in
+
+
+            (* Application des fonctions graphiques *)
+            draw_balle;
+            draw_raquette;
+            List.iter (fun (x, y) -> Graphics.fill_rect x y largeur_bloc hauteur_bloc) blocs;
+            Graphics.synchronize ();
+            (*ignore (read_line ());*)
+            ref_r := r'
+            end
+      end in
+    let handler_int i =
+      begin
+        ref_r := Flux.vide
+      end in
+    begin
+      let (inf_x, sup_x) = F.box_x in
+      let (inf_y, sup_y) = F.box_y in
+      let size_x = int_of_float (sup_x -. inf_x) in
+      let size_y = int_of_float (sup_y -. inf_y) in
+      Graphics.open_graph (Format.sprintf " %dx%d" size_x size_y);
+      Graphics.auto_synchronize false;
+      Sys.(ref_handler_alrm := signal sigalrm (Signal_handle handler_alrm));
+      Sys.(ref_handler_int  := signal sigint  (Signal_handle handler_int));
+      Unix.(setitimer ITIMER_REAL { it_interval = F.dt; it_value = F.dt })
+    end    
   end
 
 (* Caracteristiques du système:  *)
@@ -166,7 +226,7 @@ module FreeFall (F : Frame) =
 module Bouncing (F : Frame) =
   struct
 
-    include Parametres
+    include All
 
     (* version avec unfold sans récursivité directe *)
     let unless flux cond f_cond =
@@ -186,27 +246,27 @@ module Bouncing (F : Frame) =
 
     let contact_x (infx, supx) x dx = ((x -. (float_of_int rayon_ball)) <= infx && dx < 0.) || ((x +. (float_of_int rayon_ball)) >= supx && dx > 0.)
     let contact_y (infy, supy) y dy = ((y +. (float_of_int rayon_ball) >= supy && dy > 0.))
-    let contact_raq (xraq, yraq) x y dy = 
+    let contact_raq xraq x y dy = 
       dy < 0. && 
-      float_of_int yraq +. h_raq > (y -. (float_of_int rayon_ball)) && 
+      float_of_int yraq +. hraq > y && 
       float_of_int yraq < y && 
       float_of_int xraq -. long_raq /. 2. < x && 
       float_of_int xraq +. long_raq /. 2. > x
-
-    let contact_bloc blocs x dx = 
-      List.
+              
+    (*let contact_bloc blocs x dx = 
+      List.*)
 
     let rebond ((x, y), (dx, dy)) =
-      let mouse_x = fst (Graphics.mouse_pos ()) in
+      let xraq = get_xraq (fst (Graphics.mouse_pos ())) in
         (x, y),
         (
           (if contact_x F.box_x x dx then -. dx else dx),
-          (if (contact_y F.box_y y dy) || (contact_raq (mouse_x, pos_y_raq) x y dy) then -. dy else dy)
+          (if (contact_y F.box_y y dy) || (contact_raq xraq x (yb y) dy) then -. dy else dy)
         )
 
     let contact ((x, y), (dx, dy)) = 
-      let mouse_x = fst (Graphics.mouse_pos ()) in 
-      contact_x F.box_x x dx || contact_y F.box_y y dy || contact_raq (mouse_x, pos_y_raq) x y dy
+      let xraq = get_xraq (fst (Graphics.mouse_pos ())) in
+      contact_x F.box_x x dx || contact_y F.box_y y dy || contact_raq xraq x y dy
     module FF = FreeFall (F)
 
     let rec run etat0 =
@@ -215,9 +275,12 @@ module Bouncing (F : Frame) =
 
 module Init =
   struct
+
+    include All
+
     let dt = 0.01
-    let box_x = (0., 800.)
-    let box_y = (0., 600.)
+    let box_x = (0., long_ecran)
+    let box_y = (0., haut_ecran)
   end
 
 module Draw = Drawing (Init)
@@ -227,6 +290,7 @@ let _  =
   let position0 = (300., 400.) in
   let vitesse0 = (500., -200.) in
   Draw.draw (Bounce.run (position0, vitesse0));;
+
 
 
 
